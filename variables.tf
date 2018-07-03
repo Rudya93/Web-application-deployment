6cc155e12c3413c10cb9935a5662ba591b6fe88e
Rudya93 <rudyaa93@gmail.com>
2018-07-03 12:41:23 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+	default = "YOUR_ACCESS_KEY"
+}
+variable "secret_key" {
+	default = "YOUR_SECRET_KEY"
+}
+variable "region" {
+    default = "your-region"
+}
+variable "ami" {
+    default = "ami"
+}
+variable "key" {
+    default = "key"
+}
+variable "subn" {
+    default = "subn"
+}
