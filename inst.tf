57e1f12f914e82553d73e0ed621752783843d5f7
Rudya93 <rudyaa93@gmail.com>
2018-07-04 17:42:50 +0300

    Update inst.tf

--- inst.tf
+++ inst.tf
@@ -11,17 +11,11 @@ resource "aws_instance" "jenk" {
   key_name = "${var.key}"
   associate_public_ip_address = true
   private_ip = "10.244.0.20"
- 
-  # This is where we configure the instance with ansible-playbook  
-  provisioner "local-exec" {
-    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key "${var.pk}" -i '${aws_instance.jenk.private_ip},' jenk.yaml"
-    }
- 
 
-  connection {
-    user = "ubuntu"
-    private_key = "${file("${var.pk}")}"
-  }
+  # This is where we configure the instance with ansible-playbook
+  provisioner "local-exec" {
+    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key '${var.pk}' -i '${aws_instance.jenk.private_ip},' jenk.yaml"
+    }
 
   tags = {
     Name = "olrudenk_jenk"
