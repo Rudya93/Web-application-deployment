0fc2471cf56c70c22873978438f51877938b1747
Rudya93 <rudyaa93@gmail.com>
2018-07-05 10:49:03 +0300

    Update inform.py

--- inform.py
+++ inform.py
@@ -1,4 +1,4 @@
-oot@ip-10-244-0-113:~/first_app# cat inform.py                                 f = open('output.txt', 'r')
+f = open('output.txt', 'r')
 file_contents = f.read()
 print (file_contents)
 f.close()
