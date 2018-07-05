03e3660aa8e3ed6097e8a5340a507c92eb895276
Rudya93 <rudyaa93@gmail.com>
2018-07-05 11:06:43 +0300

    Create variables.tf

---
+++ Jenkins/variables.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+	default = "YOUR_ACCESS_KEY"
+}
+variable "secret_key" {
+	default = "YOUR_SECRET_KEY"
+}
+variable "region" {
+    default = "your-region"
+}
+variable "ami" {
+    default = "ami"
+}
+variable "key" {
+    default = "key"
+}
+variable "subn" {
+    default = "subn"
+}
+variable "pk" {
+    default = "/home/ubuntu/**.pem"
+}
