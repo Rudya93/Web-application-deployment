7cee7c582b81bc6caf1a2b4a5c60efcbe5a5b8a2
Rudya93 <rudyaa93@gmail.com>
2018-07-05 10:54:20 +0300

    Delete ps.py

--- ps.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import socket
-import psutil
-sys.stdout = open('output.txt','a')
-print('Host: {0} Memory: {1}' .format(socket.gethostname(), psutil.virtual_memory()))
-
